c041411db7a93b7fdaf2d1cc4b7d6d0f136c8dae
Sofia Nicoletti <sofia.nicoletti@algorand.foundation>
2026-04-29 12:54:35 +0200

    portal: redirect root to /app

--- projects/portal/app/routes/index.tsx
+++ projects/portal/app/routes/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ href: "https://algorand.co/xchain-accounts" });
+    throw redirect({ to: "/app" });
   },
 });
